0e3c1ab78c9366447ecaa715efa875bf0dcb551a
Max Bechtold <maxbechtold@web.de>
2018-06-24 13:26:45 +0200

    minor

--- migrate.sql
+++ migrate.sql
@@ -1,4 +1,4 @@
 -- Follow these migration steps if you already recorded data before updating to the mentioned commits
 
--- Commit 27bf24f9c4db47e3c82627828396e9b43509f937
+-- Commit 2cd7011ca0f84d3d2a4986363ac58d0bd2d65cc4
 ALTER TABLE laptimes ADD COLUMN timestamp INTEGER DEFAULT NULL;
