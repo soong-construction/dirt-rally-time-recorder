427ce07552c9a1acfd87ec88f487eb8c688ac4d4
Max Bechtold <maxbechtold@web.de>
2018-09-10 17:39:52 +0200

    Updated migrate.sql

--- migrate.sql
+++ migrate.sql
@@ -2,3 +2,6 @@
 
 -- Commit 2cd7011ca0f84d3d2a4986363ac58d0bd2d65cc4
 ALTER TABLE laptimes ADD COLUMN timestamp INTEGER DEFAULT NULL;
+
+-- Commit b86935b29e40311d79d0fb628a16633b96abe2f3
+UPDATE cars SET name="Delta HF Integrale", startrpm=183.25953674316406 WHERE id=14;
