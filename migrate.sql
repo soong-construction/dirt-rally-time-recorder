6caf5d22a9efbe60ca0f12adb12822a0a0fe511f
Max Bechtold <maxbechtold@web.de>
2018-09-12 16:16:41 +0200

    Simplified and updated migration guide

--- migrate.sql
+++ migrate.sql
@@ -1,8 +1,10 @@
--- Follow these migration steps if you already recorded data before updating to the mentioned commits
+-- Follow these migration steps if you already recorded data before updating to the mentioned commits. The ".<cmd>" instructions are for sqlite3, please adapt to the SQL interface you are using
 
 -- Commit 2cd7011ca0f84d3d2a4986363ac58d0bd2d65cc4
+.open dirtrally-laptimes.db
 ALTER TABLE laptimes ADD COLUMN timestamp INTEGER DEFAULT NULL;
 
--- TODO Drop & recreate?
--- Commit b86935b29e40311d79d0fb628a16633b96abe2f3
-UPDATE cars SET name="Delta HF Integrale", startrpm=183.25953674316406 WHERE id=14;
+-- Commit 34b85930f16a9e0022ffe006a699786af454189b
+.open dirtrally-lb.db
+DROP TABLE cars;
+.read cars.sql;
