b4f7d258b12c16706d9274e04d7db1660a2c756c
Max Bechtold <maxbechtold@web.de>
2018-10-07 12:40:10 +0200

    Updated migrate.sql

--- migrate.sql
+++ migrate.sql
@@ -2,3 +2,8 @@
 
 -- Commit 2cd7011ca0f84d3d2a4986363ac58d0bd2d65cc4
 ALTER TABLE laptimes ADD COLUMN timestamp INTEGER DEFAULT NULL;
+
+-- Commit 25b7b34602271e651d1ba6f7fd40a8e8a3c297b4
+.open dirtrally-lb.db
+DROP TABLE tracks;
+.read tracks.sql;
