0e0cbf47932bc5f3eb739a1dd3ad44d4239b2f38
Max Bechtold <maxbechtold@web.de>
2018-10-07 12:45:03 +0200

    minor

--- tracks.sql
+++ tracks.sql
@@ -33,9 +33,8 @@ INSERT INTO Tracks (id, name, length, startz) VALUES (31, 'Hammerstein', 10805.2
 INSERT INTO Tracks (id, name, length, startz) VALUES (32, 'Frauenberg', 11684.2207, null);
 
 -- Monte Carlo, Monaco
--- TODO migrate.sql
 INSERT INTO Tracks (id, name, length, startz) VALUES (41, 'Route de Turini', 10805.2207, 1289.7208);
-INSERT INTO Tracks (id, name, length, startz) VALUES (42, 'Valee descendante', 10866.8604, -2358.05);
+INSERT INTO Tracks (id, name, length, startz) VALUES (42, 'Vallee descendante', 10866.8604, -2358.05);
 INSERT INTO Tracks (id, name, length, startz) VALUES (43, 'Col de Turini – Sprint en descente', 4730.02, 298.587);
 INSERT INTO Tracks (id, name, length, startz) VALUES (44, 'Col de Turini sprint en Montee', 4729.54, -209.405);
 INSERT INTO Tracks (id, name, length, startz) VALUES (45, 'Col de Turini – Descente', 5175.9102, -120.206);
