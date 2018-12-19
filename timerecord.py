42270c5628248b6204c613cc835988045a2ee474
Max Bechtold <maxbechtold@web.de>
2018-12-19 15:43:31 +0100

    Removed orphaned character

--- timerecord.py
+++ timerecord.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     try:
         config = yaml.load(open(approot + '/config.yml', 'r'))
     except (yaml.YAMLError) as exc:
-        print("Error in configuration file:", exc)\
+        print("Error in configuration file:", exc)
 
     server = (config['telemetry_server']['host'], config['telemetry_server']['port'])
     speed_units = config['speed_units']
