a8c2d12736bd3d68a8ee3ac207c255e0fe5020a9
Max Bechtold <maxbechtold@web.de>
2019-06-20 13:08:12 +0200

    #6 Sketched base32 key encoding to pack client linking meta data into a URI suitable for QR Codes

--- keygen_demo.py
+++ keygen_demo.py
@@ -1,7 +1,10 @@
+from encodings.base64_codec import base64
+
+from cryptography.hazmat.backends import default_backend as crypto_default_backend
 from cryptography.hazmat.primitives import serialization as crypto_serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
-from cryptography.hazmat.backends import default_backend as crypto_default_backend
 
+UTF_8 = 'utf-8'
 key = rsa.generate_private_key(
     backend=crypto_default_backend(),
     public_exponent=65537,
@@ -16,4 +19,16 @@ public_key = key.public_key().public_bytes(
     crypto_serialization.PublicFormat.OpenSSH
 )
 
-print(public_key.decode('utf-8'))
+# TODO #6 Recreate prefix on client
+fix_prefix = 'ssh-rsa AAAAB3NzaC1y'
+
+pub_key_canon = public_key.decode(UTF_8)
+pub_key_b64 = pub_key_canon.replace(fix_prefix, '')
+print('b64 ' + pub_key_b64)
+
+pub_key_bin = base64.b64decode(bytes(pub_key_b64, UTF_8))
+pub_key_b32 = base64.b32encode(pub_key_bin).decode(UTF_8)
+print('b32 ' + pub_key_b32)
+
+pub_key_b32_url = pub_key_b32.replace("=", "%3D")
+print('b32url ' + pub_key_b32_url);
--- qrcode_demo.py
+++ qrcode_demo.py
@@ -6,15 +6,21 @@ import keygen_demo
 DATA_MODULE = '  '
 BACKGROUND = u"\u2588"u"\u2588"
 
-# TODO #6 Use base64 encoded public key? (33% overhead vs. 50% more efficient alphanumeric encoding)
 def qr_ascii_code(data):
-    qrcode = pyqrcode.create(data, 'L', 6, 'binary').text(quiet_zone=1)
-    # TODO #6 Recreate prefix on client?
-    fix_prefix = 'ssh-rsa AAAAB3NzaC1yc2E'
+    qrcode = pyqrcode.create(data, 'L', 6, 'alphanumeric').text(quiet_zone=1)
     
     qrcode_str = str(qrcode)
-    qrcode_str = qrcode_str.replace(fix_prefix, '')
     return qrcode_str.replace('1', DATA_MODULE).replace('0', BACKGROUND)
 
-qrcode = qr_ascii_code(keygen_demo.public_key)
+uri = 'DRTR://USER1234567890/V1/' + keygen_demo.pub_key_b32_url
+
+print('uri ' + uri)
+
+qrcode = qr_ascii_code(uri)
+
 print(str(qrcode))
+
+# Encode URI as such: <scheme>://<userid>/<version>/<pub_key_b32_url>, e.g. DRTR://USER1234567890/V1/ONQQAAAAAMAQAAIAAAAECAEXJF5HMFCN6RX7IESTBJG4R3OTX6B2YP6HCDEDMXDVMDETHCKMFCO2URWS546TRSYHL6RIQ3XVTDH2GCOTVIFN7JWCDB6TTQUXERJX2%3D%3D%3D
+# max length: <scheme>:// = 7, <userid>/ = 19, <version>/ = 4, <pub_key_b32_url> = 153: 183
+# allows QRCode in version 6
+# TODO #6 Truncate userid by selecting 18-suffix 
