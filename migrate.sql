de8693a37a3a231f9d7d662f018483f653ea3ca0
Max Bechtold <maxbechtold@web.de>
2019-10-18 10:05:55 +0200

    #8 Clarifying migrate.sql

--- migrate.sql
+++ migrate.sql
@@ -1,4 +1,10 @@
--- Follow these migration steps if you already recorded data before updating to the mentioned commits. The ".<cmd>" instructions are for sqlite3, please adapt to the SQL interface you are using
+-- Follow these migration steps if you already recorded data before updating to the mentioned commits. 
+-- The ".<cmd>" instructions are for sqlite3, please adapt if you are using another SQL interface  
+-- NOTE If you use the bundled version of dirt-rally-time-recorder, simply extract a newer release into your 
+-- installation directory
+
+-- TODO Migration should only be necessary with a new minor version (e.g. 1.2.0), no need to refer to commit hashes.
+-- If only dirtrally-lb.db changes, recommend migration for _every_ new version?
 
 -- ONLY if you used this tool prior to version 1.0.0
  
@@ -6,25 +12,12 @@
 .open dirtrally-laptimes.db
 ALTER TABLE laptimes ADD COLUMN timestamp INTEGER DEFAULT NULL;
 
--- Commit 34b85930f16a9e0022ffe006a699786af454189b
-.open dirtrally-lb.db
-DROP TABLE cars;
-.read cars.sql
-
--- If you used this tool prior to version 1.1.0
-
--- Commit d9574d73f07efde6dc98502cdc628164cbcd444c
-.open dirtrally-lb.db
-.read controls.sql
-
--- Commit 2a3071e64ae986c51873f68b000ca475375afed6
-.open dirtrally-lb.db
-DROP TABLE tracks;
-.read tracks.sql
-
 -- If you used this tool prior to version 1.2.0
 
 -- Commit 5846c3dae24554d017a17d7e48fb3cea18d9144e
-.open dirtrally-lb.db
-DROP TABLE controls;
-.read controls.sql
+.open --new dirtrally-lb.db
+
+.read setup-dr1.sql
+-- OR
+.read setup-dr2.sql
+
