e7ff8e711d967803e0ad36e1596dc5f72b9ae01a
Max Bechtold <maxbechtold@web.de>
2020-02-01 17:38:51 +0100

    Catch errors when reading from socket and prompt for termination

--- receiver.py
+++ receiver.py
@@ -6,6 +6,7 @@ from database import Database
 from statsProcessor import StatsProcessor
 from ambiguousResultHandler import AmbiguousResultHandler
 
+
 class Receiver(asyncore.dispatcher):
 
     def __init__(self, address, speed_unit, approot):
@@ -17,16 +18,16 @@ class Receiver(asyncore.dispatcher):
         self.finished = False
         self.track = 0
         self.car = 0
-        self.topspeed = 0 # unit: m/s
+        self.topspeed = 0  # unit: m/s
         self.previousTime = 0
         self.fieldCount = 66
-        
+
         self.database = Database(approot).setup()
         self.ambiguousResultHandler = AmbiguousResultHandler(Database.laptimesDbName)
         self.databaseAccess = DatabaseAccess(self.database, self.ambiguousResultHandler)
         self.userArray = self.database.initializeLaptimesDb()
         self.statsProcessor = StatsProcessor(self)
-        
+
         self.previousDistance = 0
         self.tracklength = -1
 
@@ -35,61 +36,71 @@ class Receiver(asyncore.dispatcher):
 
         self.create_socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.bind(self.address)
-        print("Waiting for data on %s:%s" % self.address)
+        self.print("Waiting for data on %s:%s" % self.address)
 
     def writable(self):
         return False
 
     def handle_expt(self):
-        print('exception occurred!')
+        self.print('exception occurred!')
+        self.handle_close()
+
+    def handle_error(self):
+        # TODO #17 Log errors to file and ask to contact developer
+        asyncore.dispatcher.handle_error(self)
+
+    def informCloseAndWaitForInput(self):
+        self.print('The connection was closed.')
+        input('Press ENTER to end program.')
+
+    def handle_close(self):
         self.close()
+        self.informCloseAndWaitForInput()
 
     def readable(self):
         return True
 
     def handle_read(self):
         data = self.recv(self.fieldCount * 8)
-        
+
         if not data:
             return
-        
+
         if not self.received_data:
             self.received_data = True
-            print("Receiving data on %s:%s" % self.address)
+            self.print("Receiving data on %s:%s" % self.address)
 
         self.parse(data)
-        
+
     def formatTopSpeed(self):
         topspeed_kmh = self.topspeed * 3.6
         return '%.1f' % (topspeed_kmh * self.speed_modifier,)
-    
+
     def formatLapTime(self, laptime):
         return '%.2f' % (laptime,)
 
+    # TODO #17 Log in a "python" way
     def printResults(self, laptime):
         dbAccess = self.databaseAccess
-        # FIXME Uncaught error, handle better/log. Also, test this method
-        #data = "dirtrally.%s.%s.%s.time:%f|s" % (self.userArray[0], dbAccess.identify(self.track), dbAccess.identify(self.car), self.formatLapTime(laptime))
-        ##error: uncaptured python exception, closing channel <receiver.Receiver 127.0.0.1:20778 at 0x16c613a6748> (<class 'TypeError'>:must be real number, not str [D:\Python\lib\asyncore.py|read|83] [D:\Python\lib\asyncore.py|handle_read_event|420] [G:\git-repos\dirt-rally-time-recorder\receiver.py|handle_read|60] [G:\git-repos\dirt-rally-time-recorder\receiver.py|parse|95] [G:\git-repos\dirt-rally-time-recorder\statsProcessor.py|handleGameState|19] [G:\git-repos\dirt-rally-time-recorder\receiver.py|finishStage|131] [G:\git-repos\dirt-rally-time-recorder\receiver.py|printResults|71])
         data = "dirtrally.%s.%s.%s.time:%s|s" % (self.userArray[0], dbAccess.identify(self.track), dbAccess.identify(self.car), self.formatLapTime(laptime))
-        print(data)
+        self.print(data)
         data = "dirtrally.%s.%s.%s.topspeed:%s|%s" % (self.userArray[0], dbAccess.identify(self.track), dbAccess.identify(self.car), self.formatTopSpeed(), self.speed_unit)
-        print(data)
+        self.print(data)
 
     def showCarControlInformation(self):
         if isinstance(self.car, (list,)):
             for car in self.car:
-                print(self.databaseAccess.describeCarInterfaces(car))
+                self.print(self.databaseAccess.describeCarInterfaces(car))
         else:
-            print(self.databaseAccess.describeCarInterfaces(self.car))
+            self.print(self.databaseAccess.describeCarInterfaces(self.car))
 
     def parse(self, data):
         stats = struct.unpack(str(self.fieldCount) + 'f', data[0:self.fieldCount * 4])
-        
+
         time = stats[0]
         lap = stats[59]
         distance = stats[2]
-        
+
         speed = int(stats[7])
         if self.topspeed < speed:
             self.topspeed = speed
@@ -103,29 +114,29 @@ class Receiver(asyncore.dispatcher):
     def resetStage(self):
         self.track = 0
         self.car = 0
-        
+
     def inStage(self):
         return self.track != 0 and self.car != 0
 
     def prepareStage(self):
         self.finished = False
         self.topspeed = 0
-        
+
     def startStage(self, stats):
         idle_rpm = stats[64]  # *10 to get real value
         max_rpm = stats[63]  # *10 to get real value
         z = stats[6]
         tracklength = stats[61]
-        
+
         dbAccess = self.databaseAccess
         self.track = dbAccess.identifyTrack(z, tracklength)
         self.car = dbAccess.identifyCar(idle_rpm, max_rpm)
-        
+
         self.tracklength = tracklength
-        
+
         data = "dirtrally.%s.%s.%s.started:1|c" % (self.userArray[0], dbAccess.identify(self.track), dbAccess.identify(self.car))
-        print(data)
-        
+        self.print(data)
+
         self.showCarControlInformation()
 
     def finishStage(self, stats):
@@ -133,3 +144,6 @@ class Receiver(asyncore.dispatcher):
         self.databaseAccess.recordResults(self.track, self.car, laptime)
         self.printResults(laptime)
         self.finished = True
+
+    def print(self, message):
+        print(message)
--- test_receiver.py
+++ test_receiver.py
@@ -3,50 +3,93 @@ import unittest
 from unittest.mock import MagicMock
 from receiver import Receiver
 from database import Database
+import struct
+import asyncore
+
+sixtySixFields = b'\xe9}\xb9A\x00\x00\x00\x00\x00\xf1 \xc0\x00\x00\x00\x00L\xdb(E\xcc\xde\x07D\xd1F\x87\xc5\xeep\x03<\x11\xe8\xf9\xbas\x94\xd7\xba\x0c\x99\xf9;\xeb\xe0F\xbe\xc8\nF<:\x1b{\xbfz\xd4z\xbfdfP\xbd\x88\x04F>\x99\xdd\x19@lh!?\xe9\x17f@U\x98V@\xda\xf7\x0f\xc2=\x89\x07\xc2\x86\xe6\x15\xc2D{\x0c\xc2\x00\x00\x00\x00\x00\x00\x00\x003\xb4\xb5;\x0c\x8a\xdd;\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00M\x871<,\xd3`:\x00\x00\x00\x00+b\xe6B\x00\x00\x80?\x00\x00\x80?\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xc4\xf8\xed\xc2\xc4\xf8\xed\xc2\xc4\xf8\xed\xc2\xc4\xf8\xed\xc2\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x80?\xe1\x02\xa4E\x00\x00\x00\x00`\x00iD+b\xe6B\x00\x00\xc0@'
+
 
 class TestReceiver(unittest.TestCase):
 
     def __init__(self, methodName):
         unittest.TestCase.__init__(self, methodName)
         self.DatabaseSetupFunction = Database.setup
-    
+
     def setUp(self):
         Database.setup = MagicMock()
         self.thing = Receiver(('localhost', 1024), 'kmh', 'testroot')
-        self.thing.parse = MagicMock()
 
     def tearDown(self):
         # TODO This restoration should be used throughout the test base
         Database.setup = self.DatabaseSetupFunction
-        
+        try:
+            self.thing.close()
+        except:
+            raise
+
     def testTopSpeedConversion(self):
         self.thing = Receiver(('localhost', 1024), 'kmh', 'testroot')
         self.thing.topspeed = 33.28
 
         format_top_speed = self.thing.formatTopSpeed()
         self.assertEqual(format_top_speed, '119.8')
-        
+
         self.thing = Receiver(('localhost', 1024), 'mph', 'testroot')
         self.thing.topspeed = 33.28
 
         format_top_speed = self.thing.formatTopSpeed()
         self.assertEqual(format_top_speed, '74.4')
-    
+
     def testLapTimeConversion(self):
         format_lap_time = self.thing.formatLapTime(180.249)
         self.assertEqual(format_lap_time, '180.25')
-        
-    # TODO Build integration test from here
-    def testHandleRead(self):
+
+    def testHandleReadCallsParse(self):
         self.thing.reconnect()
-        
-        sixtySixFields = b'\xe9}\xb9A\x00\x00\x00\x00\x00\xf1 \xc0\x00\x00\x00\x00L\xdb(E\xcc\xde\x07D\xd1F\x87\xc5\xeep\x03<\x11\xe8\xf9\xbas\x94\xd7\xba\x0c\x99\xf9;\xeb\xe0F\xbe\xc8\nF<:\x1b{\xbfz\xd4z\xbfdfP\xbd\x88\x04F>\x99\xdd\x19@lh!?\xe9\x17f@U\x98V@\xda\xf7\x0f\xc2=\x89\x07\xc2\x86\xe6\x15\xc2D{\x0c\xc2\x00\x00\x00\x00\x00\x00\x00\x003\xb4\xb5;\x0c\x8a\xdd;\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00M\x871<,\xd3`:\x00\x00\x00\x00+b\xe6B\x00\x00\x80?\x00\x00\x80?\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xc4\xf8\xed\xc2\xc4\xf8\xed\xc2\xc4\xf8\xed\xc2\xc4\xf8\xed\xc2\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x80?\xe1\x02\xa4E\x00\x00\x00\x00`\x00iD+b\xe6B\x00\x00\xc0@'
+
+        self.thing.parse = MagicMock()
+
         self.thing.recv = MagicMock(return_value=sixtySixFields)
-        
+
         self.thing.handle_read()
-        
+
         self.assertTrue(self.thing.parse.called, 'Never called parse')
-        
+
+    # TODO Build integration test from here
+    def testHandleFinishStageAndPrintResults(self):
+        self.thing.reconnect()
+
+        self.thing.inStage = MagicMock(return_value=True)
+        self.thing.print = MagicMock()
+        self.thing.databaseAccess.recordResults = MagicMock()
+        self.thing.databaseAccess.identifyCar = MagicMock(return_value=10)
+        self.thing.databaseAccess.identifyTrack = MagicMock(return_value=11)
+
+        stats = struct.unpack(str(self.thing.fieldCount) + 'f', sixtySixFields[0:self.thing.fieldCount * 4])
+        # stats[59] == 1 means lap/stage complete
+        stats = stats[:59] + (1.0,) + stats[60:]
+        stats = struct.pack(str(self.thing.fieldCount) + 'f', *stats)
+
+        self.thing.recv = MagicMock(return_value=stats)
+
+        self.thing.handle_read()
+
+        self.thing.print.assert_called()
+        self.thing.databaseAccess.recordResults.assert_called()
+        self.assertTrue(self.thing.finished, 'Did not toggle finished')
+
+    def testCloseGracefullyOnError(self):
+        self.thing.reconnect()
+
+        self.thing.recv = MagicMock(return_value=sixtySixFields)
+        self.thing.parse = MagicMock(side_effect=TypeError)
+        self.thing.informCloseAndWaitForInput = MagicMock()
+
+        asyncore.read(self.thing)
+
+        self.thing.informCloseAndWaitForInput.assert_called()
+
+
 if __name__ == "__main__":
-    #import sys;sys.argv = ['', 'TestReceiver.testTopSpeedConversion']
+    # import sys;sys.argv = ['', 'TestReceiver.testTopSpeedConversion']
     unittest.main()
