ec09fd772a781427315f945c5ee20c38f1e980db
Max Bechtold <maxbechtold@web.de>
2020-04-01 17:38:20 +0200

    #25 let GearTracker handle neutral and reverse properly

--- gearTracker.py
+++ gearTracker.py
@@ -1,5 +1,5 @@
 class GearTracker():
-    
+
     def __init__(self):
         self.lastGear = None
         self.changeCount = 0
@@ -9,7 +9,6 @@ class GearTracker():
         if current_gear != self.lastGear:
             self.changeCount += 1
 
-    # TODO Must handle Reverse = 10.0 correctly
     # TODO Must hande Recover Vehicle correctly
     def checkGearSkipped(self, current_gear):
         distance = self.lastGear - current_gear
@@ -19,12 +18,20 @@ class GearTracker():
 
     def track(self, stats):
         current_gear = stats[33]
+        # Ignore neutral = 0
+        if current_gear == 0:
+            return
+
+        # Handle reverse gear = -1 (DR1: 10.0)
+        if current_gear == -1 or current_gear == 10:
+            current_gear = 0
+
         if self.lastGear is not None:
             self.checkGearChanged(current_gear)
             self.checkGearSkipped(current_gear)
-            
+
         self.lastGear = current_gear
-            
+
     def getGearChangeCount(self):
         return self.changeCount
 
