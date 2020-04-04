28fb9bb8a562d5e001d50eeafa3b4c47ca1391d1
Max Bechtold <maxbechtold@web.de>
2020-04-04 10:35:24 +0200

    #25 Only respawns at stage start are recognized as restarts

--- respawnTracker.py
+++ respawnTracker.py
@@ -1,9 +1,10 @@
 
+recoverDelta = 2
+restartDelta = 30
+restartDistance = 10
+
 # Should handle most respawns, except successive recovers in one place (only DR1)
-# TODO test
 class RespawnTracker():
-    recoverDelta = 2
-    restartDelta = 30
     
     def __init__(self):
         self.last_x = None
@@ -15,18 +16,27 @@ class RespawnTracker():
         self.last_x = pos_x
         self.last_y = pos_y
 
+    def positionDeltaExceeds(self, pos_x, pos_y, delta):
+        exceeds_x = abs(pos_x - self.last_x) >= delta
+        exceeds_y = abs(pos_y - self.last_y) >= delta
+        
+        return exceeds_x or exceeds_y
+
     def track(self, stats):
         self.recover = False
         self.restart = False
         pos_x = stats[4]
         pos_y = stats[5]
+        distance = stats[2]
         
         if self.last_x is None:
             pass
-        elif abs(pos_x - self.last_x) > self.restartDelta or abs(pos_y - self.last_y) > self.restartDelta:  
+        
+        elif self.positionDeltaExceeds(pos_x, pos_y, restartDelta) and distance <= restartDistance:
             self.restart = True
             print('[RESTART]')
-        elif abs(pos_x - self.last_x) > self.recoverDelta or abs(pos_y - self.last_y) > self.recoverDelta:  
+                
+        elif self.positionDeltaExceeds(pos_x, pos_y, recoverDelta):  
             self.recover = True
             print('[RECOVER]')
 
---
+++ test_respawnTracker.py
@@ -0,0 +1,138 @@
+import unittest
+from respawnTracker import RespawnTracker
+
+fieldCount = 66
+
+class TestRespawnTracker(unittest.TestCase):
+
+    def __init__(self, methodName):
+        unittest.TestCase.__init__(self, methodName)
+
+    def setUp(self):
+        self.thing = RespawnTracker()
+
+    def tearDown(self):
+        pass
+    
+    def testNoRespawnForFirstStats(self):
+        stats = [0] * fieldCount
+        stats[4] = 100.0
+        
+        self.thing.track(stats)
+        self.assertFalse(self.thing.isRecover() or self.thing.isRestart())
+        
+    def testNoRespawnForLowXDeltas(self):
+        stats = [0] * fieldCount
+        stats[4] = 100.0
+        self.thing.track(stats)
+    
+        stats[4] = 101.1
+        self.thing.track(stats)
+        self.assertFalse(self.thing.isRecover() or self.thing.isRestart())
+        
+        stats[4] = 100.8
+        self.thing.track(stats)
+        self.assertFalse(self.thing.isRecover() or self.thing.isRestart())
+        
+        stats[4] = 99.9
+        self.thing.track(stats)
+        self.assertFalse(self.thing.isRecover() or self.thing.isRestart())
+        
+    def testNoRespawnForLowYDeltas(self):
+        stats = [0] * fieldCount
+        stats[5] = 100.0
+        self.thing.track(stats)
+        
+        stats[5] = 101.1
+        self.thing.track(stats)
+        self.assertFalse(self.thing.isRecover() or self.thing.isRestart())
+        
+        stats[5] = 100.8
+        self.thing.track(stats)
+        self.assertFalse(self.thing.isRecover() or self.thing.isRestart())
+        
+        stats[5] = 99.9
+        self.thing.track(stats)
+        self.assertFalse(self.thing.isRecover() or self.thing.isRestart())
+        
+    def testNoRespawnForCombinedDeltas(self):
+        stats = [0] * fieldCount
+        stats[4] = 100.0
+        stats[5] = 100.0
+        self.thing.track(stats)
+        
+        stats[4] = 101.1
+        stats[5] = 101.0
+        self.thing.track(stats)
+        self.assertFalse(self.thing.isRecover() or self.thing.isRestart())
+        
+        stats[4] = 100.8
+        stats[5] = 102.2
+        self.thing.track(stats)
+        self.assertFalse(self.thing.isRecover() or self.thing.isRestart())
+
+    def testSmallDeltaIsRecover(self):
+        stats = [0] * fieldCount
+        stats[4] = 100.0
+        stats[5] = 100.0
+        self.thing.track(stats)
+        
+        stats[4] = 95.0
+        stats[5] = 100.0
+        self.thing.track(stats)
+        self.assertTrue(self.thing.isRecover())
+        self.assertFalse(self.thing.isRestart())
+        
+        stats[4] = 96.8
+        stats[5] = 99.9
+        self.thing.track(stats)
+        self.assertFalse(self.thing.isRecover())
+        self.assertFalse(self.thing.isRestart())
+
+        stats[4] = 97.0
+        stats[5] = 105.0
+        self.thing.track(stats)
+        self.assertTrue(self.thing.isRecover())
+        self.assertFalse(self.thing.isRestart())
+
+    def testLargeDeltaIsRestartForDistanceValueNearZero(self):
+        stats = [0] * fieldCount
+        stats[2] = 13
+        stats[4] = 100.0
+        stats[5] = 100.0
+        self.thing.track(stats)
+        
+        stats[2] = 5
+        stats[4] = 20.0
+        stats[5] = 100.0
+        self.thing.track(stats)
+        self.assertFalse(self.thing.isRecover())
+        self.assertTrue(self.thing.isRestart())
+
+        stats[4] = 10.0
+        stats[5] = 15.0
+        self.thing.track(stats)
+        self.assertFalse(self.thing.isRecover())
+        self.assertTrue(self.thing.isRestart())        
+        
+    def testLargeDeltaIsRecoverForHigherDistanceValue(self):
+        stats = [0] * fieldCount
+        stats[2] = 25
+        stats[4] = 100.0
+        stats[5] = 100.0
+        self.thing.track(stats)
+        
+        stats[4] = 20.0
+        stats[5] = 100.0
+        self.thing.track(stats)
+        self.assertTrue(self.thing.isRecover())
+        self.assertFalse(self.thing.isRestart())
+
+        stats[4] = 10.0
+        stats[5] = 15.0
+        self.thing.track(stats)
+        self.assertTrue(self.thing.isRecover())
+        self.assertFalse(self.thing.isRestart())        
+        
+if __name__ == '__main__':
+    unittest.main()
