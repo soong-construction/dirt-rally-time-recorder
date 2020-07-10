66751d482c65cb7699617c217ef41c4e227cbc9c
Max Bechtold <maxbechtold@web.de>
2020-07-10 17:23:23 +0200

    # 45 Sign bundled exe with included certificate

--- resources/bundle-dr1.bat
+++ resources/bundle-dr1.bat
@@ -1,3 +1,5 @@
 Del /F dirtrally-lb.db
 bin\sqlite3 -batch -init resources\setup-dr1.sql dirtrally-lb.db .exit 
 pyinstaller timerecord.spec --noconfirm --name timerecorder-dr1
+pwsh.exe -wd resources resources\sign.ps1 -Exe ..\dist\timerecorder-dr1\timerecord.exe
+EXIT /B %ERRORLEVEL%
---
+++ resources/sign.ps1
@@ -0,0 +1,19 @@
+param ($Exe = $(throw "Exe parameter is required."))
+
+## Create new certificate (one-time)
+# New-SelfSignedCertificate -Subject "CN=soong.construction.dev@gmail.com; O=github.com/soong-construction" -NotAfter (Get-Date).AddMonths(48) -Type CodeSigning -CertStoreLocation cert:\CurrentUser\My
+## Export to password protected file (one-time)
+# $mypwd = ConvertTo-SecureString -Force -AsPlainText
+# (Get-ChildItem Cert:\CurrentUser\My -CodeSigningCert)[0] | Export-PfxCertificate -FilePath code_signing.pfx -Password $mypwd
+## or unprotected, or use certmgr.msc
+# Export-Certificate -Cert (Get-ChildItem Cert:\CurrentUser\My -CodeSigningCert)[0] -FilePath code_signing.crt
+## Short circuit locally
+# $DRTR_CODESIGN_CERTIFICATE_PASS = ConvertFrom-SecureString -SecureString $mypwd
+
+## Apply it in CI (PWSH 6+)
+try {
+    $cert = Get-PfxCertificate -FilePath .\code_signing.pfx -Password (ConvertTo-SecureString -String $DRTR_CODESIGN_CERTIFICATE_PASS)
+    Set-AuthenticodeSignature $Exe -Certificate $cert
+} catch {
+    exit 1
+}
