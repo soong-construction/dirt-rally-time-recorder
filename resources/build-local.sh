0ac1f54211c9aad40a74452907a022380e61f83d
Max Bechtold <maxbechtold@web.de>
2020-08-16 17:55:39 +0200

    #36 Improve build-local.sh

--- resources/build-local.sh
+++ resources/build-local.sh
@@ -4,16 +4,15 @@ rm -f dirtrally-lb.db
 ./bin/sqlite3 -batch -init resources/setup-${game}.sql dirtrally-lb.db .exit 
 pyinstaller timerecord.spec --noconfirm --name timerecorder-${game}
 
-# Linux only
 rm dist/**/*.pyd
 rm dist/**/*.zip
+rm dist/**/*.dll
 rm dist/**/timerecord.exe*
-rm dist/**/python*.dll
 cp --parents timerecorder/*.py dist/timerecorder-${game}
 
 pushd dist/timerecorder-${game}
 mv list-laptimes.bat list-laptimes.sh
-cp export-laptimes.bat export-laptimes.sh
+mv export-laptimes.bat export-laptimes.sh
 # Reduce scripts to core parts
 sed -i '1d;$d' *-laptimes.sh
 popd
