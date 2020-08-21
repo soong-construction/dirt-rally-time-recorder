b07bc142b6464d6f28f7d4df40b7467c12041f2b
Max Bechtold <maxbechtold@web.de>
2020-08-21 10:27:11 +0200

    #45 Set exe version for Windows to display

--- resources/bundle-dr1.ps1
+++ resources/bundle-dr1.ps1
@@ -2,6 +2,6 @@ if (Test-Path dirtrally-lb.db) {
   Remove-Item -Force dirtrally-lb.db
 }
 bin\sqlite3 -batch -init resources\setup-dr1.sql dirtrally-lb.db .exit 
-pyinstaller timerecord.spec --noconfirm --name timerecorder-dr1
+pyinstaller --clean --noconfirm --version-file=resources\version_info.txt timerecord.spec --name test
 pwsh.exe -wd resources resources\sign.ps1 -Exe ..\dist\timerecorder-dr1\timerecord.exe
 exit $LASTEXITCODE
