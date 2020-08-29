5bfc8501111660d27dc181f674007b9224d9673d
Max Bechtold <maxbechtold@web.de>
2020-08-29 12:14:30 +0200

    #47 Update SQLite, improve database output

--- resources/export-laptimes.sql
+++ resources/export-laptimes.sql
@@ -1,12 +1,13 @@
--- Export your times with SQLite  
+-- Export your times with SQLite
 .open dirtrally-laptimes.db
 attach 'dirtrally-lb.db' as base;
 .separator ";"
+.headers on
 .once snapshot.csv
-select t.name, 
-		c.name, 
-		strftime('%Y-%m-%d %H:%M:%S', datetime(l.timestamp, 'unixepoch', 'localtime')) as timestamp,
-		strftime('%M:%f', l.time, 'unixepoch'),
-		l.topspeed
+select t.name as Track,
+		c.name as Car,
+		strftime('%Y-%m-%d %H:%M:%S', datetime(l.timestamp, 'unixepoch', 'localtime')) as Date,
+		strftime('%M:%f', l.time, 'unixepoch') as Time,
+		l.topspeed as Topspeed
 from base.tracks t, base.cars c, laptimes l
 where t.id=l.track and c.id=l.car;
--- resources/list-laptimes.sql
+++ resources/list-laptimes.sql
@@ -1,12 +1,12 @@
--- List your times with SQLite  
+-- List your times with SQLite
 .open dirtrally-laptimes.db
 attach 'dirtrally-lb.db' as base;
-.mode column
-select t.name, 
-		c.name, 
-		strftime('%Y-%m-%d %H:%M:%S', datetime(l.timestamp, 'unixepoch', 'localtime')) as timestamp,
-		strftime('%M:%f', l.time, 'unixepoch'),
-		l.topspeed
+.mode box
+select t.name as Track,
+		c.name as Car,
+		strftime('%Y-%m-%d %H:%M:%S', datetime(l.timestamp, 'unixepoch', 'localtime')) as Date,
+		strftime('%M:%f', l.time, 'unixepoch') as Time,
+		l.topspeed as Topspeed
 from base.tracks t, base.cars c, laptimes l
 where t.id=l.track and c.id=l.car
 order by t.name asc, c.name asc, timestamp desc;
