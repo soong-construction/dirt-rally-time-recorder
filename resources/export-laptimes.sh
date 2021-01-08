17d9615bd44b5929cabb9c07f07ac20227c46ace
Max Bechtold <maxbechtold@web.de>
2021-01-08 14:26:59 +0100

    Removing sh scripts which are created by build-local.sh

--- resources/export-laptimes.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sqlite3 -init export-laptimes.sql dirtrally-laptimes.db .exit
-echo Exported to snapshot.csv
-read -s -n 1 -p "Press any key..."
--- resources/list-laptimes.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sqlite3 -init list-laptimes.sql dirtrally-laptimes.db .exit
-read -s -n 1 -p "Press any key..."
